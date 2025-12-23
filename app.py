56a2a3b4065995d4f943fdb3de34dca94a1722fb
tasueom <tasueom@naver.com>
2025-12-23 11:32:12 +0900

    chore: Load environment variables and update app.run debug mode based on DEBUG environment variable

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from app import create_app
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(debug=False)
+    app.run(debug=os.environ.get('DEBUG', 'True').lower() == 'true')
